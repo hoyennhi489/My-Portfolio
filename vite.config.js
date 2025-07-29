60b2d0a5bcc67fa4b8f2bd6355ed25e340a3b82d
hoyennhi489 <hyn489@gmail.com>
2025-07-29 16:51:49 +0700

    Rebuild project after fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Portfolio/',
   plugins: [react()],
-    server: {
-    host: true
-  }
+  base: '/My-Portfolio/'
 })
